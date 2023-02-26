1d3e401e594f5bde9a54794c22f1a6290b1fe986
leocjacob <leocjacob@gmail.com>
2023-02-25 19:35:14 -0500

    implemented all functionalities

---
+++ pricing-service/src/main/java/com/udacity/pricing/PricingServiceApplication.java
@@ -0,0 +1,17 @@
+package com.udacity.pricing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+/**
+ * Creates a Spring Boot Application to run the Pricing Service.
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PricingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PricingServiceApplication.class, args);
+	}
+
+}
